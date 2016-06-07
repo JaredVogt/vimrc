35894b169678a26c36e5f23103f7907cc1129ab2
Jared Vogt <jared.vogt@gmail.com>
2016-06-06 19:42:19 -0700

    start of script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-ls
+curl -Lo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -Lo ~/.vim/colors/monokai.vim --create-dirs https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim
+curl -O https://raw.githubusercontent.com/JaredVogt/vimrc/master/.vimrc
